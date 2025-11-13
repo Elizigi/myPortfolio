ac737f653a086ac58ea1adb4772d044555e28378
Elizigi <elizigi876@gmail.com>
2025-11-13 19:33:15 +0200

    refactor/added grandma mode

--- resume/src/components/sentenceWriter/SentenceWriter.tsx
+++ resume/src/components/sentenceWriter/SentenceWriter.tsx
@@ -3,14 +3,16 @@ import styles from "./SentenceWriter.module.scss";
 import UseSentenceWriter from "../../hooks/SentenceWriterHook";
 
 const SentenceWriter = () => {
-  const { typedWord } = UseSentenceWriter(sentencesModelArray);
+  const { typedWord, isBreathing } = UseSentenceWriter(sentencesModelArray);
   return (
     <div className={styles.sentenceContainer}>
       <div className={styles.currentWord}>
         <h2>{typedWord}</h2>
       </div>
-      <div className={styles.lShape}>
-        <h2>{"l"}</h2>
+      <div
+        className={`${styles.lShape} ${isBreathing ? styles.breathing : ""}`}
+      >
+        <h2>|</h2>
       </div>
     </div>
   );
--- resume/src/hooks/SentenceWriterHook.tsx
+++ resume/src/hooks/SentenceWriterHook.tsx
@@ -1,46 +1,68 @@
 import { useEffect, useRef, useState } from "react";
 
-const UseSentenceWriter = (sentencesArray: string[]) => {
+const UseSentenceWriter = (
+  sentencesArray: string[],
+  grandmaMode: boolean = false
+) => {
   const [typedWord, setTypedWord] = useState("");
+  const [isBreathing, setIsBreathing] = useState(false);
+
   const currentLetterIdx = useRef(0);
   const selectedSentence = useRef(0);
   const timeDelay = useRef(50);
   const timeToRead = 2000; //milliseconds
   const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
-  const minTypeDelay = 70;
-  const maxLetterVariation = 150;
+  const timeoutBreatheRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const breathDelay = 350;
+  const minTypeDelay = grandmaMode ? 800 : 30;
+  const maxLetterVariation = grandmaMode ? 1200 : 150;
 
   const chooseWord = () => {
     selectedSentence.current = Math.floor(
       Math.random() * sentencesArray.length
     );
+    setIsBreathing(false);
     console.log(selectedSentence.current);
     setTypedWord("");
-
     currentLetterIdx.current = 0;
+
     timeoutRef.current = setTimeout(() => {
       addLetter();
     }, timeDelay.current);
   };
 
+  const breathe = () => {
+    setIsBreathing(true);
+  };
+
+  const stopBreathing = () => {
+    setIsBreathing(false);
+  };
+
   useEffect(() => {
     chooseWord();
+    breathe()
     return () => {
       if (timeoutRef.current) clearTimeout(timeoutRef.current);
+      if (timeoutBreatheRef.current) clearTimeout(timeoutBreatheRef.current);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const addLetter = () => {
+    stopBreathing();
     timeDelay.current =
       minTypeDelay + Math.floor(Math.random() * maxLetterVariation);
+
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    if (timeoutBreatheRef.current) clearTimeout(timeoutBreatheRef.current);
 
     const sentence = sentencesArray[selectedSentence.current];
 
     const positionToAdd = currentLetterIdx.current;
     setTypedWord((perv) => perv + sentence[positionToAdd]);
     currentLetterIdx.current++;
-
+    timeoutBreatheRef.current = setTimeout(() => breathe(), breathDelay);
     if (sentence.length === currentLetterIdx.current) {
       if (timeoutRef.current) clearTimeout(timeoutRef.current);
       timeoutRef.current = null;
@@ -48,10 +70,11 @@ const UseSentenceWriter = (sentencesArray: string[]) => {
       setTimeout(() => chooseWord(), timeToRead);
       return;
     }
+
     timeoutRef.current = setTimeout(() => addLetter(), timeDelay.current);
   };
 
-  return { chooseWord, typedWord };
+  return { typedWord, isBreathing };
 };
 
 export default UseSentenceWriter;
